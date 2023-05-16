37e1252ac6b00070d73e4c414528acaa72ec99fc
Aditya Godse <adimail2404@gmail.com>
2023-05-16 23:54:26 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,11 +1,11 @@
 <?php
 // Establish a connection to the database
 $servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_database_name";
+$username = "root";
+$password = "";
+$dbname = "sign up";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect('locolhost', 'root', '', 'sign up');
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
@@ -16,11 +16,13 @@ $name = $_POST['name'];
 $birthday = $_POST['birthday'];
 $location = $_POST['location'];
 $qualification = $_POST['qualification'];
+$number = $_POST['number'];
+$password = $_POST['password'];
 
 // Sanitize and validate the data if necessary
 
 // Insert the data into the database
-$sql = "INSERT INTO users (name, birthday, location, qualification) VALUES ('$name', '$birthday', '$location', '$qualification')";
+$sql = "INSERT INTO users (name, birthday, location, qualification, number, password) VALUES ('$name', '$birthday', '$location', '$qualification', '$number', '$password')";
 
 if (mysqli_query($conn, $sql)) {
     echo "Data submitted successfully";
